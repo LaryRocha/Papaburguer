4c7f2813b6f8c7da586af116bda5634d6a4c4851
LaryRocha <lary_web@hotmail.com>
2015-06-30 15:58:56 -0300

    Inserindo Funções

--- framework/Database.php
+++ framework/Database.php
@@ -1,6 +1,17 @@
+<?php
+
 namespace framework
 
 class Database {
-
-
+	
+	public function insert ($cargo, $campos, $dados){
+		$this->executarComando("INSERT INTO {$tabela}(". implode(',',$campos).") VALUES (". implode(',',$dados).");");
+	}
 }
+
+$db = new Database($conexao);
+
+$db->insert('cargo', ['nome'], ['Gerente']);
+$db->insert('cargo', ['nome'], ['Administrador']);
+$db->insert('cargo', ['nome'], ['Garçon']);
+?>
