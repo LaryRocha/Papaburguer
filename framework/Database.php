167ae524b3e5a3e25540d123cc2b9535df9453bb
LaryRocha <lary_web@hotmail.com>
2015-06-30 17:36:21 -0300

    Segunda Tentativa - Funções

--- framework/Database.php
+++ framework/Database.php
@@ -12,10 +12,13 @@ class Database {
 		$this->executeSql("INSERT INTO {$table}(". implode(',',$fields).") VALUES (". implode(',',$datas).");");
 	}
 	public function update($table, $fields, $datas){
+		$this->executeSql("UPDATE {$table} SET ------ = ------- WHERE id = {$id}");
 	}
 	public function delete($table, $id){
+		$this->executeSql("DELETE FROM {$table} WHERE id = {$id}");
 	}
 	public function select($table, $fields, $where){
+		$this->executeSql("SELECT FROM {$table} WHERE id = {$id}");
 	}	
 
 }
