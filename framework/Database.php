350da4f0f9eeb65b88ba71675fea46d4a5d76e20
Anderson S Rocha <andersonrocha@maxfs.com>
2015-07-06 18:38:37 -0300

    update function

--- framework/Database.php
+++ framework/Database.php
@@ -3,29 +3,34 @@
 namespace framework
 
 class Database {
-	
+
 	public function conect($dsn){
-		$conectar = mysql_connect('localhost', 'mysql_user', 'mysql_password'); 
+		$conectar = mysql_connect('localhost', 'mysql_user', 'mysql_password');
 	}
 	private function executeSql($sql){
-		if(!$conectar){ 
-			die('Não foi possível conectar: ' . mysql_error()); 
-		} 
-		echo 'Conexão bem sucedida'; 
+		if(!$conectar){
+			die('Não foi possível conectar: ' . mysql_error());
+		}
+		echo 'Conexão bem sucedida';
 		mysql_close($conectar);
 	}
 	public function insert ($table, $fields, $datas){
 		$this->executeSql("INSERT INTO {$table}(". implode(',',$fields).") VALUES (". implode(',',$datas).");");
 	}
-	public function update($table, $fields, $datas){
-		$this->executeSql("UPDATE {$table} SET ------ = ------- WHERE id = {$id}");
+	public function update($table, $fields, $datas, $where){
+		$updateQuery = []
+		foreach($fields as $id => $field){
+			array_push($updateQuery, "{$field} = '{$datas[$id]}'");
+		}
+		$updateQuery = implode(',',$updateQuery);
+		$this->executeSql("UPDATE {$table} SET {$updateQuery} WHERE $where");
 	}
 	public function delete($table, $id){
-		$this->executeSql("DELETE FROM {$table} WHERE id = {$id}"); 
+		$this->executeSql("DELETE FROM {$table} WHERE id = {$id}");
 	}
 	public function select($table, $fields, $where){
-		$this->executeSql("SELECT FROM {$table} $where id = {$id}"); 
-	}	
+		$this->executeSql("SELECT FROM {$table} $where id = {$id}");
+	}
 
 }
 
